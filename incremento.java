4d41c88626bd21563eb9e33a43dbf3f51f2008eb
KarinaSelingardi <133906857+KarinaSelingardi@users.noreply.github.com>
2023-07-28 10:44:24 -0300

    Add files via upload

---
+++ incremento.java
@@ -0,0 +1,17 @@
+/**operador de incremento**/
+public class Incremento {
+	
+	public static void main (String[]args ){
+		int x = 8;
+		int y = ++x;//pré-incremento
+		int b = 4;
+		int a = b++;//pós-incremento
+		
+		
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+		System.out.println("b=" + b);
+		System.out.println("a=" +a);
+	}//fim do metodo
+} // fim da classe
+		
---
+++ operações_java.java
@@ -0,0 +1,17 @@
+/** operadores matematicos em java*/
+
+public class Operador {
+	/*metodo principal da classe*/
+	public static void main (string[] args){
+		double x = 7 - 3
+		double y = -7 - 3
+		double z = 7 * 3
+		double a = 7 % 2
+		double b = -6
+		System.out.println("x");
+		System.out.println("y");
+		System.out.println("z");
+		System.out.println("a");
+		System.out.println("b");
+	}//fim do metodo
+}//fim da classe
