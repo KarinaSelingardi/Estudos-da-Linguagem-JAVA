f6243122bcbebd877db1c0079f46e4885ac3a0cd
KarinaSelingardi <133906857+KarinaSelingardi@users.noreply.github.com>
2023-07-28 16:40:11 -0300

    Add files via upload

---
+++ OperadorLogicoE.java
@@ -0,0 +1,14 @@
+/**
+ * classe utilizada para demonstrar o uso do operador logico E (&&)
+ */
+ 
+ public class OperadorLogicoE {
+     public static void main (String[]args){
+         boolean a =  true;
+         boolean b = false;
+         boolean c = true;
+         System.out.println(a && b);
+         System.out.println(a && c);
+         
+     }
+ }
